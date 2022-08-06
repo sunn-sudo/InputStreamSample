cd985e00aef24f3dbf686dc397998908a1f93329
sunn <sanpu712@gmail.com>
2022-08-07 00:56:54 +0900

    ファイル出力処理

--- src/sample/OutputStreamSample.java
+++ src/sample/OutputStreamSample.java
@@ -8,16 +8,51 @@ import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.io.IOUtils;
+
 /** ファイル作成処理サンプル */
 public class OutputStreamSample {
 
 	public static void main(String[] args) throws IOException, URISyntaxException {
+		OutputStreamSample001();
+		OutputStreamSample002();
+	}
+
+	/**
+	 * 手法１
+	 * 
+	 * @throws IOException
+	 */
+	private static void OutputStreamSample001() throws IOException {
 		// ファイルの中身を記載
 		String exampleString = "Hello World";
 		InputStream inputStream = new ByteArrayInputStream(exampleString.getBytes(StandardCharsets.UTF_8));
 
 		// 嫌がらせのため大量のファイルを生成する
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 100; i++) {
+			// ファイル生成
+			FileOutputStream outputStream = new FileOutputStream(new File("バカ" + i + "号.txt"));
+			// ファイル書き込み処理
+			int mojiIndex;
+			while ((mojiIndex = inputStream.read()) != -1) {
+				char getSingleChar = (char) mojiIndex;
+				outputStream.write(getSingleChar);
+			}
+		}
+	}
+
+	/**
+	 * 手法2
+	 * 
+	 * @throws IOException
+	 */
+	private static void OutputStreamSample002() throws IOException {
+		// ファイルの中身を記載
+		String exampleString = "Hello World";
+		InputStream inputStream = IOUtils.toInputStream(exampleString);
+
+		// 嫌がらせのため大量のファイルを生成する
+		for (int i = 100; i < 200; i++) {
 			// ファイル生成
 			FileOutputStream outputStream = new FileOutputStream(new File("バカ" + i + "号.txt"));
 			// ファイル書き込み処理
